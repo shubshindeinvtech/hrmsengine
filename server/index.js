280b0c78cc7fb3ba8e8c20e40d74454b4b1f7e7e
sauravdhaka <shubham.shinde@invezzatechnologies.com>
2025-02-03 14:13:47 +0530

    Update CORS configuration to specify allowed origins for the application

--- server/index.js
+++ server/index.js
@@ -16,12 +16,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-const allowedOrigins = ['*'];
+const allowedOrigins = [
+  "http://192.168.1.37:5173",
+  "http://localhost:5173",
+  "http://172.30.32.1:5173",
+  "https://invezzahrms.shub.space",
+  "https://engineinv.vercel.app",
+  "/*",
+];
 
 const corsOptions = {
-  origin: "http://www.hrmsdev.invezzatech.com/register",
+  origin: allowedOrigins,
   methodS: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  allowedOrigins: ['Content-type', 'Authorization'],
   credentials: true,
 };
 
